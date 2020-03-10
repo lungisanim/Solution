2ddaa9ed29085b278e073b2afb816ffa7b30134d
Lungisani Nhlengethwa <lungisani.nhlengethwa@redpandasoftware.co.za>
2020-03-10 12:05:52 +0200

    Primary commit

---
+++ Consola/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Consola
+{
+    class Program { 
+        static void Main(string[] args)
+        {
+            string s = "Wow Example!";
+            int n = 12;
+
+
+            Console.WriteLine(Utilities.jumbledString(s, n));
+        }
+    }
+}
---
+++ Consola/Utilities.cs
@@ -0,0 +1,21 @@
+﻿namespace Consola
+{
+    class Utilities
+    {
+        public static string jumbledString(string s, long n)
+        {
+            string evenChars = string.Empty, oddChars = string.Empty;
+
+            if (string.IsNullOrEmpty(s))
+                return null;
+
+            for (int i = 0; i < s.Length; i++)
+                if (i % 2 == 0)
+                    evenChars += s[i];
+                else
+                    oddChars += s[i];
+
+            return evenChars + oddChars;
+        }
+    }
+}
